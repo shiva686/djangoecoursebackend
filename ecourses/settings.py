2c877bd444035e1f7b41dbe23a9711bedab8fe6e
shiva686 <smartlearn795@gmail.com>
2020-09-01 18:57:06 +0530

    final upload

--- ecourses/settings.py
+++ ecourses/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a%vdohxroqpsk0+^a0&qb+q3jj7l7hgaae&6pg=6%4)a2!0rqm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ "https://djangoecourses.herokuapp.com/"]
+ALLOWED_HOSTS = [ "https://djangoecourses.herokuapp.com/" , "https://ecourses-53958.web.app/"]
 
 
 # Application definition
